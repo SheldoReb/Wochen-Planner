07b7a28fa615efca37d264a2e9c80d789ae46d08
Rebernik <sheldon.rebernik@munichmotorsport.de>
2024-10-13 01:52:43 +0200

    Update environment configuration and integrate dotenv for better management of environment variables

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,47 +1,54 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import session from 'express-session';
-import cors from 'cors';
-import recipeRoutes from './routes/recipeRoutes.mjs';
-import cuisineRoutes from './routes/cuisineRoutes.mjs'; // Import the new cuisine routes
+import express from 'express'
+import mongoose from 'mongoose'
+import session from 'express-session'
+import cors from 'cors'
+import recipeRoutes from './routes/recipeRoutes.mjs'
+import cuisineRoutes from './routes/cuisineRoutes.mjs'
+import dotenv from 'dotenv'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+dotenv.config()
 
-mongoose.connect('mongodb://sheldor:Sheldon0@192.168.178.19:27778/meal-planner', {
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const mongoUri = process.env.MONGODB_URI || 'mongodb://sheldor:Sheldon0@192.168.178.19:27778/meal-planner'
+
+mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  authSource: 'admin' // or the database where the user is created
+  authSource: 'admin'
 }).then(() => {
-  console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB')
 }).catch((error) => {
-  console.error('Error connecting to MongoDB:', error.message, error.stack);
-});
+  console.error('Error connecting to MongoDB:', error.message, error.stack)
+})
+
+const corsOrigin = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost', 'http://localhost:5173']
 
 app.use(cors({
-  origin: ['http://localhost', 'http://localhost:5173'], // Allow multiple origins
+  origin: corsOrigin,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
-}));
+}))
 
-app.use(express.json());
+app.use(express.json())
 app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  next();
-});
+  console.log(`Incoming request: ${req.method} ${req.url}`)
+  next()
+})
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
-}));
+}))
 
-app.use('/api/recipes', recipeRoutes);
-app.use('/api', cuisineRoutes); // Add the new route for cuisines
+app.use('/api/recipes', recipeRoutes)
+app.use('/api', cuisineRoutes)
 
 app.listen(PORT, (err) => {
   if (err) {
-    console.error(`Failed to start server: ${err.message}`, err.stack);
+    console.error(`Failed to start server: ${err.message}`, err.stack)
   } else {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`)
   }
-});
+})
